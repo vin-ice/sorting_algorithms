bcd34599b2da3a9e49e7f78d5a8487376736f9b3
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-24 22:14:57 +0300

    102-corr

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,19 +8,19 @@
 void counting_sort(int *array, size_t size)
 {
 	int max, count, *c_arr, *out;
-	/**
-	 * 1. find the max value in input array
-	 * 2. create an array of len(max+1): c_arr: init all ele to 0
-	 * 3. accumulate c_arr ele
-	 * 4. order input 
-	*/
+	
+	if (size < 2 || !array)
+		return;
+
 	for (max=array[0], count=0; count < (int)size; count++)
 		if (array[count] > max)
 			max = array[count];
+	
 	out = malloc(sizeof(int) * size);
 	c_arr = malloc(sizeof(int) * max + 1);
 	if (c_arr == NULL || out == NULL)
 		return;
+	
 	for (count = 0; count <= max; count++) /**init*/
 		c_arr[count] = 0;
 	for (count = 0; count < (int)size; count++)/**count ele in input*/
