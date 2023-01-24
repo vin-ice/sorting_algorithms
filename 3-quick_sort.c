37290efb697f64b1d7f4ac9230b6ea641c4dfc34
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-24 21:59:45 +0300

    3-cor

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,72 +1,71 @@
 #include "sort.h"
-
 /**
- * swap - swaps two values at two addresses
- * r - address of right position
- * l - address of left position
-*/
-void swap(int *r, int *l)
-{
-    int temp;
-
-    temp = *r;
-    *r = *l;
-    *l = temp;
-}
-/**
- * partition - lomuto partition scheme
- * @arr: Array to partition
- * @lo: lowest index of given array
- * @hi:  Highest index of array
- * 
- * Return: returns pivot point
-*/
-int partition(int *array, int lo, int hi, size_t size)
-{
-    int pivot, i, j;
-
-    pivot = array[hi];/**last element*/
-
-    for (i = lo - 1, j = lo; j < hi; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i += 1;
-            swap((array + i), (array + j));
-        }
-    }
-    i += 1;
-    swap((array + i), (array + j));
-    print_array(array, size);
-    return (i);
-}
-/**
- * sort - inner implementation of the sorting algo
- * @arr: Pointer to array rto sort
- * @l: left most index
- * @r: right most index
-*/
-void sort(int *arr, int l, int r, size_t size)
-{
-    int pivot;
-
-    if (l >= r || l < 0)
-        return;
-    
-    pivot = partition(arr, l, r, size);
-    sort(arr, l, pivot - 1, size);
-    sort(arr, pivot + 1, r, size);
-
-}
-/**
- * quick_sort - sorts an array of integers in asc
- * order using the quick sort algo
- * @array: array
- * @size: size of array
-*/
+  * quick_sort - quicksort algorithm
+  * @array: array to be sorted
+  * @size: size of array
+  */
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2 || array == NULL)
-        return;
-    sort(array, 0, size - 1, size);
+	if (array == NULL || size <= 1)
+		return;
+	sort(array, 0, size - 1, size);
+}
+
+/**
+  * sort - recursive sorting algorithm
+  * @arr: array
+  * @left: leftmost index
+  * @right: rightmost index
+  * @size: full size of array
+  */
+void sort(int *arr, int left, int right, size_t size)
+{
+	int p;
+
+	if (left < right)
+	{
+		p = split(arr, left, right, size);
+		sort(arr, left, p - 1, size);
+		sort(arr, p + 1, right, size);
+	}
+}
+
+/**
+  * split - split array
+  * @arr: array
+  * @left: leftmost index
+  * @right: rightmost index
+  * @size: full array size
+  * Return: pivot index
+  */
+int split(int *arr, int left, int right, size_t size)
+{
+	int i, i2, pivot, tmp;
+
+	pivot = arr[right];
+	i = left;
+
+	for (i2 = left; i2 < right; i2++)
+	{
+		if (arr[i2] < pivot)
+		{
+			if (i != i2)
+			{
+				tmp = arr[i2];
+				arr[i2] = arr[i];
+				arr[i] = tmp;
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[right])
+	{
+		tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		print_array(arr, size);
+	}
+
+	return (i);
 }
